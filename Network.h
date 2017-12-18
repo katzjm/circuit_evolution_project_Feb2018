9aa91803a04b7563779e168b99ee5234b75095d8
katzjm <katzjm@uw.edu>
2017-12-18 15:45:28 -0800

    made header for SetNetwork

--- Network.h
+++ Network.h
@@ -1,6 +1,7 @@
 /*
  * A Network represents a group of reactions that governs how different
- * reagents interact with one another
+ * reagents interact with one another. Note that a Network can support
+ * MAX_NUM_REACTIONS at a time
  *
  * Author: Josh Katz
  */
@@ -21,9 +22,13 @@ typedef struct network_str {
 // Parameters:
 //      network - Pointer to the network to fill with reactions
 //      max_num_reactions - The maximum number of reactions that can be
-//                          in this network
+//                          in this network. If this number is greater
+//                          than MAX_NUM_REACTIONS then MAX_NUM_REACTIONS
+//                          is used
 //      min_num_reactions - The minimum number of reactions that can be
-//                          in this network
+//                          in this network. If this number is greater
+//                          than MAX_NUM_REACTIONS then MAX_NUM_REACTIONS
+//                          is used
 //      num_reagents - The number of reagents that can be in this network
 //      max_rate_constant - The largest rate constant any reaction can have
 //      min_rate_constant - The smallest rate constant any reaction can have
@@ -34,4 +39,12 @@ void SetRandomNetwork(Network_Ptr network,
                       float max_rate_constant,
                       float min_rate_constant);
 
-
+// Fills the given Network with copies of the Reactions in reactions
+//
+// Parameters:
+//      network - Pointer to the network to fill with reactions
+//      reactions - Pointer to an array of reactions copy to network
+//      num_reactions - The number of reactions in the given reaction array.
+//                      Note that if this is greater than MAX_NUM_REACTIONS than
+//                      only the first MAX_NUM_REACTIONS are copied into network
+void SetNetwork(Network_Ptr network, Reaction_Ptr reactions, int num_reactions);
