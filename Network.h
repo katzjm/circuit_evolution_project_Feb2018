58f62e965658ece3d130255d0db8e9f22ae3cf9c
katzjm <katzjm@uw.edu>
2017-12-18 15:35:32 -0800

    made SetRandomNetwork

--- Network.h
+++ Network.h
@@ -9,7 +9,29 @@
 
 #define MAX_NUM_REACTIONS 30
 
+// Memory block for maintaining a network of Reactions
 typedef struct network_str {
-    
+    Reaction reactions[MAX_NUM_REACTIONS];
     int num_reactions;
-    
+} Network, *Network_Ptr;
+
+// Fills the given network with a random number of random reactions with
+// constraints outlined in the other parameters
+//
+// Parameters:
+//      network - Pointer to the network to fill with reactions
+//      max_num_reactions - The maximum number of reactions that can be
+//                          in this network
+//      min_num_reactions - The minimum number of reactions that can be
+//                          in this network
+//      num_reagents - The number of reagents that can be in this network
+//      max_rate_constant - The largest rate constant any reaction can have
+//      min_rate_constant - The smallest rate constant any reaction can have
+void SetRandomNetwork(Network_Ptr network,
+                      int max_num_reactions,
+                      int min_num_reactions,
+                      int num_reagents,
+                      float max_rate_constant,
+                      float min_rate_constant);
+
+
