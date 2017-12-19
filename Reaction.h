b40ce86d2ddedfd62b48387f67b5d5d9c134ee0f
katzjm <katzjm@uw.edu>
2017-12-18 21:57:22 -0800

    Reaction takes config structs now

--- Reaction.h
+++ Reaction.h
@@ -4,6 +4,8 @@
  * Author: Josh Katz
  */
 
+#include "Configs.h"
+
 #define NO_REAGENT -1
 
 typedef struct reaction_str {
@@ -19,95 +21,45 @@ typedef struct reaction_str {
 //
 // Parameters:
 //      reaction - Pointer to the reaction to change
-//      num_reagents - The number of reagents that can be involved in the
-//                     reaction. Note that the actual number of reagents
-//                     will be between 1 and num_reagents
-//      max_rate_constant - The largest reaction rate that this reaction
-//                          can have
-//      min_rate_constant - The smallest reaction rate that this reaction
-//                          can have. Note that the actual reaction is
-//                          equally likely to be of any order of magnitude
-//                          between min_rate_constant and max_reaction_rate
-void SetRandomReaction(Reaction_Ptr reaction,
-                       int num_reagents,
-                       float max_rate_constant,
-                       float min_rate_constant);
+//      config - Pointer to a struct containting configuration parameters.
+//               See Config.h
+void SetRandomReaction(Reaction_Ptr reaction, RConfig_Ptr config);
 
 // Returns a Reaction with one random reactant and one random product (UniUni),
 // and a randomly selected reaction rate
 //
 // Parameters:
 //      reaction - Pointer to the reaction to change
-//      num_reagents - The number of reagents that can be involved in the
-//                     reaction. Note that the actual number of reagents
-//                     will be between 1 and num_reagents
-//      max_rate_constant - The largest reaction rate that this reaction
-//                          can have
-//      min_rate_constant - The smallest reaction rate that this reaction
-//                          can have. Note that the actual reaction is
-//                          equally likely to be of any order of magnitude
-//                          between min_rate_constant and max_reaction_rate
-void SetRandomUniUni(Reaction_Ptr reaction,
-                     int num_reagents,
-                     float max_rate_constant,
-                     float min_rate_constant);
+//      config - Pointer to a struct containting configuration parameters.
+//               See Config.h
+void SetRandomUniUni(Reaction_Ptr reaction, RConfig_Ptr config);
 
 // Returns a Reaction with two random reactants and one random product (BiUni),
 // and a randomly selected reaction rate
 //
 // Parameters:
 //      reaction - Pointer to the reaction to change
-//      num_reagents - The number of reagents that can be involved in the
-//                     reaction. Note that the actual number of reagents
-//                     will be between 1 and num_reagents
-//      max_rate_constant - The largest reaction rate that this reaction
-//                          can have
-//      min_rate_constant - The smallest reaction rate that this reaction
-//                          can have. Note that the actual reaction is
-//                          equally likely to be of any order of magnitude
-//                          between min_rate_constant and max_reaction_rate
-void SetRandomBiUni(Reaction_Ptr reaction,
-                    int num_reagents,
-                    float max_rate_constant,
-                    float min_rate_constant);
+//      config - Pointer to a struct containting configuration parameters.
+//               See Config.h
+void SetRandomBiUni(Reaction_Ptr reaction, RConfig_Ptr config;
 
 // Returns a Reaction with one random reactant and two random products (UniBi),
 // and a randomly selected reaction rate
 //
 // Parameters:
 //      reaction - Pointer to the reaction to change
-//      num_reagents - The number of reagents that can be involved in the
-//                     reaction. Note that the actual number of reagents
-//                     will be between 1 and num_reagents
-//      max_rate_constant - The largest reaction rate that this reaction
-//                          can have
-//      min_rate_constant - The smallest reaction rate that this reaction
-//                          can have. Note that the actual reaction is
-//                          equally likely to be of any order of magnitude
-//                          between min_rate_constant and max_reaction_rate
-void SetRandomUniBi(Reaction_Ptr reaction,
-                    int num_reagents,
-                    float max_rate_constant,
-                    float min_rate_constant);
+//      config - Pointer to a struct containting configuration parameters.
+//               See Config.h
+void SetRandomUniBi(Reaction_Ptr reaction, RConfig_Ptr config);
 
 // Returns a Reaction with two random reactants and two random products (BiBi),
 // and a randomly selected reaction rate
 //
 // Parameters:
 //      reaction - Pointer to the reaction to change
-//      num_reagents - The number of reagents that can be involved in the
-//                     reaction. Note that the actual number of reagents
-//                     will be between 1 and num_reagents
-//      max_rate_constant - The largest reaction rate that this reaction
-//                          can have
-//      min_rate_constant - The smallest reaction rate that this reaction
-//                          can have. Note that the actual reaction is
-//                          equally likely to be of any order of magnitude
-//                          between min_rate_constant and max_reaction_rate
-void SetRandomBiBi(Reaction_Ptr reaction,
-                   int num_reagents,
-                   float max_rate_constant,
-                   float min_rate_constant);
+//      config - Pointer to a struct containting configuration parameters.
+//               See Config.h
+void SetRandomBiBi(Reaction_Ptr reaction, RConfig_Ptr config);
 
 // Returns a Reaction with given reactants, products, and reaction rate
 //
@@ -133,6 +85,6 @@ void SetReaction(Reaction_Ptr reaction,
 //
 // Parameters:
 //      reaction - A pointer to the reaction to modify
-//      max_percent_change - The maximum magnitude percent change of
-//                           the reaction
-void MutateRateConstant(Reaction_Ptr reaction, float max_percent_change);
+//      config - Pointer to a struct containting configuration parameters.
+//               See Config.h
+void MutateRateConstant(Reaction_Ptr reaction, RConfig_Ptr config);
