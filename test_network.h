9ee35fd13e03b897af95dfde907c9c9b6ea20ac2
katzjm <katzjm@uw.edu>
2017-12-28 21:04:12 -0800

    test_network header file made

---
+++ test_network.h
@@ -0,0 +1,39 @@
+/*
+ * Tests that Network works correctly
+ *
+ * Author: Josh Katz
+ */
+
+#ifndef _EVOLVER_TEST_NETWORK_H_
+#define _EVOLVER_TEST_NETWORK_H_
+
+#include "gtest/gtest.h"
+
+extern "C" {
+  #include "Network.h"
+  #include "Reaction.h"
+  #include "Configs.h"
+}
+
+namespace evolvertest {
+
+class Test_Network : public ::testing::Test {
+ protected:
+  Test_Network() {
+    Configure(&c, NULL);
+  }
+
+  virtual ~Test_Network() {}
+
+//  virtual void SetUp() {}
+
+//  virtual void TearDown() {}
+
+  Config c;
+  Network network;
+
+};  // Test_Network
+
+}  // evolvertest
+
+#endif // _EVOLVER_TEST_Network_H_
